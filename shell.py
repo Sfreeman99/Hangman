2ba5e08c84005073fea0365b5c2438a8e209a427
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-09-06 15:37:47 -0500

    [Shell] added a function to change the letters into underlines to not reveal the answer

--- shell.py
+++ shell.py
@@ -1,3 +1,14 @@
+def underline(phrase):
+    ''' str -> Nonetype
+
+    Takes in a string and changes it into '__'
+
+    >>> underline('Hello')
+    '__ __ __ __ __'
+    '''
+    return ('__ ' * len(phrase)).strip()
+
+
 print('Welcome to Hangman')
 phrases = ['Hello']
-print(phrases)
+print(current_phrase)
